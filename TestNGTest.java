486eacbab56857122991fc801d7961ec9da684e3
Yess Espinosa <yessmetal@gmail.com>
2022-11-11 21:59:05 -0600

    Add files via upload

---
+++ TestNGTest.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class TestNGTest {
+	@Test
+	public void TestMethod() {
+		System.out.println("testing method");
+	}
+}
---
+++ holamundo.java
@@ -0,0 +1,9 @@
+
+public class holamundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
+	}
+
+}
